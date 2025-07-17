da42f9029e41c9001c74cc9a6d6814b787485376
Thomas ten Cate <ttencate@gmail.com>
2025-07-17 16:02:29 +0200

    Add an example main scene for testing export

--- example_extension/src/lib.rs
+++ example_extension/src/lib.rs
@@ -1,6 +1,26 @@
+use godot::classes::{ISprite2D, Sprite2D};
 use godot::prelude::*;
 
 struct ExampleExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for ExampleExtension {}
+
+/// A sprite that spins around its origin at a fixed speed.
+#[derive(GodotClass)]
+#[class(init, base = Sprite2D)]
+struct Spinning {
+    /// The speed at which the sprite spins, in radians per second.
+    #[export]
+    angular_speed: f32,
+
+    base: Base<Sprite2D>,
+}
+
+#[godot_api]
+impl ISprite2D for Spinning {
+    fn physics_process(&mut self, delta: f32) {
+        let angle_delta = self.angular_speed * delta;
+        self.base_mut().rotate(angle_delta);
+    }
+}
